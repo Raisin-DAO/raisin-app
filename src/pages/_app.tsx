4c378253790d84b4f4d63d049d62d7d766320635
Saúl García Céspedes <saul@dixwe.com>
2023-05-17 23:01:46 +0200

    feat: initial nextjs project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { goerli } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [goerli],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID as string }),
+    publicProvider(),
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,82 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useContract, useContractRead } from 'wagmi';
+import abi from '../abi.json';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+const CONTRACT_ADDRESS = '0x5d0cB8c5C90A714B4Ce2891cf8CC20fc2A1bA04F';
+
+export default function Home() {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  const { data } = useContractRead({
+    address: CONTRACT_ADDRESS,
+    abi: abi,
+    functionName: 'greet',
+    args: [],
+  });
+  const greeting = (data as string) || 'Loading';
+
+  // This is to get around the infamous 'Hydration failed' error. See: https://codingwithmanny.medium.com/understanding-hydration-errors-in-nextjs-13-with-a-web3-wallet-connection-8155c340fbd5
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) return null;
+
+  return (
+    <main className='py-20 px-32'>
+      <h1 className='text-3xl font-bold'>🪄 create-web3-frontend</h1>
+      <div className='mt-4'>
+        <ConnectButton />
+      </div>
+
+      <hr className='mt-8 border border-slate-200' />
+
+      <div className='mt-8'>
+        <p>
+          Just fetched the greeting: <b>{greeting}</b> from{' '}
+          <Link
+            href={`https://goerli.etherscan.io/address/${CONTRACT_ADDRESS}`}
+            className='underline'
+          >
+            {CONTRACT_ADDRESS}
+          </Link>
+        </p>
+
+        <p className='mt-4'>
+          This project has been bootstrapped with Next.js, TailwindCSS, wagmi,
+          and Rainbowkit.
+        </p>
+
+        <p className='mt-4'>
+          Refer the{' '}
+          <Link className='underline' href='https://wagmi.sh'>
+            wagmi docs
+          </Link>{' '}
+          to learn how to use wagmi to read and write information from the
+          blockchain.
+        </p>
+
+        <p className='mt-4'>
+          Start editing <code>src/pages/index.tsx</code> to get started!
+        </p>
+      </div>
+
+      <hr className='mt-8 border border-slate-200' />
+
+      <div className='mt-8 flex gap-6'>
+        <Link
+          className='underline'
+          href='https://github.com/Dhaiwat10/create-web3-frontend'
+        >
+          Star this repo on GitHub ⭐️
+        </Link>
+
+        <Link className='underline' href='https://twitter/dhaiwat10'>
+          Follow me on Twitter 🐦
+        </Link>
+      </div>
+    </main>
+  );
+}
