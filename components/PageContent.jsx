258a29537d265918d7ff4c02cc20fb9530f083e6
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-05-22 08:07:57 +0530

    Feature : Developed landing page content section #5

--- components/PageContent.jsx
+++ components/PageContent.jsx
@@ -3,7 +3,7 @@ import PageContentCard from './PageContentCard'
 function PageContent() {
     return (
         <>
-            <div className='bg-black text-white flex flex-col justify-around lg:px-60 py-10 pt-40 relative' style={{ clipPath: "polygon(0 20%, 100% 0, 100% 100%, 0% 100%)" }} >
+            <div className='bg-black text-white flex flex-col justify-around px-60 pb-20 pt-36 page-content-clip-path'  >
                 <div className='w-1/2 mb-20'>
                     <h1 className='text-3xl mb-10'>Something</h1>
                     <p className='text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius modi odio ex quia cum laboriosam, tempora minus nisi. Inventore debitis laborum id laboriosam at alias veniam sit atque recusandae temporibus eveniet ea dolore, ex libero, distinctio obcaecati vero aut aperiam!</p>
